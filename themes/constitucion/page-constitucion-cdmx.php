46be2c8ecd5a304284a6386e3f1d6cb452ec8f3b
Nayeli Jordan <raul@zamacona.me>
2016-02-23 13:13:04 -0600

    carpetas bootstrap

--- themes/constitucion/page-constitucion-cdmx.php
+++ themes/constitucion/page-constitucion-cdmx.php
@@ -1,8 +1,20 @@
-<?php get_header(); the_post(); 
+<?php get_header(); the_post();
 
 the_title(); ?>
 
 
 <?php get_sidebar(); ?>
-
+<section class="[ bg-image rectangle-small ][ margin-bottom ][ relative ]" style="background-image: url('<?php echo THEMEPATH; ?>/img/hero-video.jpg');">
+						<h2 class=" [ text-uppercase ][ center-full ]">Información</h2>
+<!-- 	<img class="[ center-block ][ width-50 ]" src="<?php echo THEMEPATH; ?>/img/icons/play.png"> -->
+</section>
+<section class="[ container ]">
+	<h2>La constitución</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae amet aperiam pariatur ea iste officia facilis, eligendi rem modi reiciendis delectus impedit quibusdam consectetur possimus eaque nihil facere reprehenderit nisi.</p>
+	<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+	<img class="[ img-responsive ]" src="<?php echo THEMEPATH; ?>/images/edificios.png">
+</section>
+<section>
+	<h2>Proceso participativo</h2>
+</section>
 <?php get_footer(); ?>
--- themes/constitucion/page-home.php
+++ themes/constitucion/page-home.php
@@ -1,6 +1,20 @@
+		<video width="100%" height="300" controls></video>
+		<button class="[ btn-play ][ center-full ]" onclick="playPause()"><img src="<?php echo THEMEPATH; ?>images/icons/play-btn.png"></button>
 <?php get_header(); the_post();  ?>
 
-home -- Favor de Leer el archivo readme.md
+
+
+<!--home -- Favor de Leer el archivo readme.md -->
+
+			<div class="[ container ]">
+				<div class="[ row ]">
+					<div class="[ col-xs-12 ]">
+						<h1 class="[ ]">ConstituciónCDMX</h1>
+						<h2 class="[ ]">Nulla sed aliquet purus. Sed fringilla facilisis sapien, et iaculis nibh condimentum at. </h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac dolor nec nulla consectetur lobortis vel nec eros. Aliquam erat volutpat. Sed at tempor nisi. Phasellus suscipit feugiat libero, sit amet dictum turpis maximus sollicitudin. Maecenas quis scelerisque ligula, at hendrerit turpis. Nulla sed aliquet purus. Sed fringilla facilisis sapien, et iaculis nibh condimentum at. Proin ut lorem quis magna dapibus ultricies nec sit amet ex. Vivamus convallis urna in iaculis cursus. Suspendisse venenatis rhoncus commodo. Curabitur et ornare lorem, nec ornare turpis. Nulla ultricies nibh suscipit efficitur congue.</p>
+					</div>
+				</div>
+			</div><!--/container-->
 
 <?php get_sidebar(); ?>
 

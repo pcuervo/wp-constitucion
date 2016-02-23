a334b309c3333d35887bb901e770e48972b45983
Nayeli Jordan <raul@zamacona.me>
2016-02-23 15:41:05 -0600

    cdmx sin graficas, sin container body

--- themes/constitucion/page-cdmx.php
+++ themes/constitucion/page-cdmx.php
@@ -1,8 +1,45 @@
-<?php get_header(); the_post(); 
+<?php get_header(); the_post();
 
 the_title(); ?>
 
 
 <?php get_sidebar(); ?>
+<section>
+	<div class="[ bg-image rectangle-small ][ margin-bottom ][ relative ]" style="background-image: url('<?php echo THEMEPATH; ?>/images/edificios.png');">
+		<div class="[ bg-dark--opacity width-100 height-100 ]">
+			<h2 class=" [ text-uppercase ][ no-margin ][ center-full ][ color-light ][ fw-semibold ]">Ciudad de México</h2>
+		</div>
+	</div>
+</section>
+<section class="[ container ]">
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae amet aperiam pariatur ea iste officia facilis, eligendi rem modi reiciendis delectus impedit quibusdam consectetur possimus eaque nihil facere reprehenderit nisi.</p>
+	<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+	<img class="[ img-responsive ][ margin-auto ]" src="<?php echo THEMEPATH; ?>/images/edificios.png">
+	<div class="[ row ]">
+		<div class="[ col-xs-12 col-sm-6 ]">
+
+		</div>
+		<div class="[ col-xs-12 col-sm-6 ]">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae amet aperiam pariatur ea iste officia facilis, eligendi rem modi reiciendis delectus impedit quibusdam consectetur possimus eaque nihil facere reprehenderit nisi.</p>
+		</div>
+	</div>
+	<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+	<div class="[ row ]">
+		<div class="[ col-xs-4 col-sm-3 ]">
+			<img class="[ img-responsive ][ margin-auto ]" src="<?php echo THEMEPATH; ?>/images/edificios.png">
+		</div>
+		<div class="[ col-xs-4 col-sm-3 ]">
+			<img class="[ img-responsive ][ margin-auto ]" src="<?php echo THEMEPATH; ?>/images/edificios.png">
+		</div>
+		<div class="[ col-xs-4 col-sm-3 ]">
+			<img class="[ img-responsive ][ margin-auto ]" src="<?php echo THEMEPATH; ?>/images/edificios.png">
+		</div>
+		<div class="[ hidden-xs col-sm-3 ]">
+			<img class="[ img-responsive ][ margin-auto ]" src="<?php echo THEMEPATH; ?>/images/edificios.png">
+		</div>
+	</div>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque ducimus provident voluptatum accusamus, reprehenderit adipisci repellendus est doloribus exercitationem possimus obcaecati quia minima blanditiis, sequi facilis earum voluptates. Tenetur, et.</p>
+	<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+</section>
 
 <?php get_footer(); ?>

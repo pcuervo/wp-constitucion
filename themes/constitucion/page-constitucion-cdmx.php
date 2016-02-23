ab78ae5ee8c27425017f9ccf7bdb9ff9b72af0c3
Nayeli Jordan <raul@zamacona.me>
2016-02-23 13:37:35 -0600

    primera parte constitucion- cdmx

--- themes/constitucion/page-constitucion-cdmx.php
+++ themes/constitucion/page-constitucion-cdmx.php
@@ -12,9 +12,42 @@ the_title(); ?>
 	<h2>La constitución</h2>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae amet aperiam pariatur ea iste officia facilis, eligendi rem modi reiciendis delectus impedit quibusdam consectetur possimus eaque nihil facere reprehenderit nisi.</p>
 	<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, ipsum! Eaque voluptatum inventore delectus? Optio perspiciatis ullam excepturi hic eius repellat placeat alias debitis numquam perferendis, est asperiores consectetur corporis!</p>
 	<img class="[ img-responsive ]" src="<?php echo THEMEPATH; ?>/images/edificios.png">
 </section>
-<section>
+<section class="[ container ]">
 	<h2>Proceso participativo</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, reiciendis, facilis! Cupiditate necessitatibus praesentium ipsam officia dolore id esse, enim fuga maiores dignissimos adipisci laudantium consequatur, obcaecati debitis illum corrupti.</p>
+	<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores.</p>
+	<div class="[ row ]">
+		<div class="[ col-xs-6 col-sm-3 ]">
+			<h3>Paso 1</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit culpa ipsam quibusdam similique autem, quam, fugit vel veritatis velit consectetur incidunt dignissimos nihil, adipisci soluta mollitia magnam! Recusandae nisi, necessitatibus!</p>
+		</div>
+		<div class="[ col-xs-6 col-sm-3 ]">
+			<h3>Paso 2</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae veniam sapiente consectetur, eum saepe accusamus commodi cumque possimus ipsum ex est explicabo, nam eveniet corporis odio tempora neque doloremque iure?</p>
+		</div>
+		<div class="[ clearfix ][ visible-xs ]"></div>
+		<div class="[ col-xs-6 col-sm-3 ]">
+			<h3>Paso 3</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel mollitia neque, aspernatur molestiae earum accusamus veniam facilis aliquid placeat dolorum eos ullam eligendi possimus at, recusandae ducimus fuga beatae. Quo.</p>
+		</div>
+		<div class="[ col-xs-6 col-sm-3 ]">
+			<h3>Paso 4</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint ipsam qui labore, mollitia pariatur natus ab veniam, facilis quaerat facere repellendus nobis suscipit quae in inventore, architecto deleniti voluptatibus. Quidem!</p>
+		</div>
+	</div>
+	<div>
+		<p class="[ inline-block middle ][ text-uppercase ]">Descarga los documentos oficiales aquí 	</p>
+		<img class="[ inline-block middle ]" src="<?php echo THEMEPATH; ?>/icons/.png">
+	</div>
+</section>
+<section class="[ container ]">
+	<h2>experiencias y buenas prácticas globales</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis deserunt, quidem totam eveniet expedita assumenda illum accusantium iusto ratione, ex atque molestias consectetur facere, reiciendis blanditiis id suscipit rerum, libero?</p>
+	<p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.</p>
+	<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+
 </section>
 <?php get_footer(); ?>

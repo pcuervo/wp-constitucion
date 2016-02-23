2bff31edbbaae7975a2e2a71a327815212f1eda5
Nayeli Jordan <raul@zamacona.me>
2016-02-23 17:33:24 -0600

    cdmx sin graficas

--- themes/constitucion/js/functions.js
+++ themes/constitucion/js/functions.js
@@ -4,42 +4,8 @@
 
 	$(function(){
 
-			/**
-		 * home
-		**/
-		if (is_cdmx == 1) {
-			/**
-			 * On ready
-			**/
 
-			/**
-			 * Triggered events
-			**/
 
-			//Chart.js
-
-			//#mycanvas
-			var ctx = $("#mycanvas").get(0).getContext("2d");
-			//pie chart data
-			//sum of values = 360
-			var data = [
-				{
-					value: 46,
-					color: "#f99134",
-					highlight: "#f96234",
-					label: "Local"
-				},
-				{
-					value: 54,
-					color: "#fff",
-					highlight: "#fff",
-					label: ""
-				}
-			];
-			//draw
-			var piechart = new Chart(ctx).Pie(data);
-
-		};
 
 	});
 
--- themes/constitucion/page-constitucion-cdmx.php
+++ themes/constitucion/page-constitucion-cdmx.php
@@ -4,87 +4,114 @@ the_title(); ?>
 
 
 <?php get_sidebar(); ?>
-<section>
-	<div class="[ bg-image rectangle-small ][ margin-bottom ][ relative ]" style="background-image: url('<?php echo THEMEPATH; ?>/images/edificios.png');">
-		<div class="[ bg-dark--opacity width-100 height-100 ]">
-			<h2 class=" [ text-uppercase ][ no-margin ][ center-full ][ color-light ][ fw-semibold ]">Información</h2>
-		</div>
+<section class="[ bg-image rectangle-small ][ margin-bottom--large ][ relative ]" style="background-image: url('<?php echo THEMEPATH; ?>/images/edificios.png');">
+	<div class="[ bg-dark--opacity width-100 height-100 ]">
+		<h2 class=" [ text-uppercase ][ no-margin ][ center-full ][ color-light ][ fw-semibold ]">Información</h2>
 	</div>
 </section>
-
-<section class="[ container ]">
-	<h2>La constitución</h2>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae amet aperiam pariatur ea iste officia facilis, eligendi rem modi reiciendis delectus impedit quibusdam consectetur possimus eaque nihil facere reprehenderit nisi.</p>
-	<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, ipsum! Eaque voluptatum inventore delectus? Optio perspiciatis ullam excepturi hic eius repellat placeat alias debitis numquam perferendis, est asperiores consectetur corporis!</p>
+<section class="[ container padding--sides--xsm ][ margin-bottom--large ]">
+	<div class="[ margin-bottom--large ]">
+		<h2>La constitución</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae amet aperiam pariatur ea iste officia facilis, eligendi rem modi reiciendis delectus impedit quibusdam consectetur possimus eaque nihil facere reprehenderit nisi.</p>
+		<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, ipsum! Eaque voluptatum inventore delectus? Optio perspiciatis ullam excepturi hic eius repellat placeat alias debitis numquam perferendis, est asperiores consectetur corporis!</p>
+	</div>
 	<img class="[ img-responsive ][ margin-auto ]" src="<?php echo THEMEPATH; ?>/images/edificios.png">
 </section>
-<article class="[ bg-gray-light ][ margin-top-bottom--large padding--top-bottom--large ]">
-	<section class="[ container ]">
+<article class="[ bg-gray-light ][ margin-top-bottom--large padding--top-bottom--xlarge ]">
+	<section class="[ container padding--sides--xsm ]">
 		<h2 class="[ no-margin-top ]">Proceso participativo</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, reiciendis, facilis! Cupiditate necessitatibus praesentium ipsam officia dolore id esse, enim fuga maiores dignissimos adipisci laudantium consequatur, obcaecati debitis illum corrupti.</p>
 		<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores.</p>
-		<div class="[ row ][ margin-bottom ]">
+		<div class="[ row ][ margin-top-bottom ]">
 			<div class="[ col-xs-6 col-sm-3 ]">
-				<p class="[ fz-xlarge ]">Paso 1</p>
-				<p class="[ fz-small ]">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+				<div class="[ row ]">
+					<div class="[ col-xs-8 ]">
+						<p class="[ fz-xlarge ]">Paso 1</p>
+						<p class="[ fz-small ]">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+					<div class="[ col-xs-4 ]">
+						<img class="[ img-responsive ][ margin-auto ]" src="<?php echo THEMEPATH; ?>/images/flecha.png">
+					</div>
+				</div>
 			</div>
 			<div class="[ col-xs-6 col-sm-3 ]">
-				<p class="[ fz-xlarge ]">Paso 2</p>
-				<p class="[ fz-small ]">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+				<div class="[ row ]">
+					<div class="[ col-xs-8 ]">
+						<p class="[ fz-xlarge ]">Paso 2</p>
+						<p class="[ fz-small ]">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+					<div class="[ col-xs-4 ]">
+						<img class="[ img-responsive ][ margin-auto ]" src="<?php echo THEMEPATH; ?>/images/flecha.png">
+					</div>
+				</div>
 			</div>
 			<div class="[ clearfix ][ visible-xs ]"></div>
 			<div class="[ col-xs-6 col-sm-3 ]">
-				<p class="[ fz-xlarge ]">Paso 3</p>
-				<p class="[ fz-small ]">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+				<div class="[ row ]">
+					<div class="[ col-xs-8 ]">
+						<p class="[ fz-xlarge ]">Paso 3</p>
+						<p class="[ fz-small ]">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+					<div class="[ col-xs-4 ]">
+						<img class="[ img-responsive ][ margin-auto ]" src="<?php echo THEMEPATH; ?>/images/flecha.png">
+					</div>
+				</div>
 			</div>
 			<div class="[ col-xs-6 col-sm-3 ]">
-				<p class="[ fz-xlarge ]">Paso 4</p>
-				<p class="[ fz-small ]">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+				<div class="[ row ]">
+					<div class="[ col-xs-8 ]">
+						<p class="[ fz-xlarge ]">Paso 4</p>
+						<p class="[ fz-small ]">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+					<div class="[ col-xs-4 ]">
+						<img class="[ img-responsive ][ margin-auto ]" src="<?php echo THEMEPATH; ?>/images/flecha.png">
+					</div>
+				</div>
 			</div>
 		</div>
-		<div class="">
+		<div>
 			<p class="[ inline-block middle ][ text-uppercase ]">Descarga los documentos oficiales aquí 	</p>
-			<img class="[ inline-block middle ]" src="<?php echo THEMEPATH; ?>/icons/.png">
+			<img class="[ svg icon icon--iconed--xlarge icon--thickness-2 icon--fill ][ color-primary ]"  src="<?php echo THEMEPATH; ?>icons/file-acrobat.svg">
 		</div>
 	</section>
 </article>
 
-<section class="[ container ]">
+<section class="[ container padding--sides--xsm ][ margin-bottom--large ]">
 	<h2>Experiencias y buenas prácticas globales</h2>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis deserunt, quidem totam eveniet expedita assumenda illum accusantium iusto ratione, ex atque molestias consectetur facere, reiciendis blanditiis id suscipit rerum, libero?</p>
 	<p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.</p>
 	<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
 </section>
 <section class="[ bg-image rectangle ][ margin-bottom ][ relative ]" style="background-image: url('<?php echo THEMEPATH; ?>/images/edificios.png');"></section>
-<section class="[ container ][ margin-bottom--large ]">
-	<h2>Preguntas frecuentes</h2>
+<section class="[ container padding--sides--xsm ][ margin-bottom--large ]">
+	<h2 class="[ margin-bottom ]">Preguntas frecuentes</h2>
 	<div class="[ row ]">
-		<div class="[ col-xs-12 col-sm-6 ]">
+		<div class="[ col-xs-12 col-sm-6 ][ margin-bottom ]">
 			<p class="[ text-uppercase ]"><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</strong></p>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt neque, fugiat nostrum. Tempora aliquid quasi blanditiis doloribus deserunt natus obcaecati non. Recusandae ipsa fugit molestias error eum repellat qui omnis.</p>
 		</div>
-		<div class="[ col-xs-12 col-sm-6 ]">
-			<p class="[ text-uppercase ]"><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</strong></p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt neque, fugiat nostrum. Tempora aliquid quasi blanditiis doloribus deserunt natus obcaecati non. Recusandae ipsa fugit molestias error eum repellat qui omnis.</p>
-		</div>
-	</div>
-		<div class="[ row ]">
-		<div class="[ col-xs-12 col-sm-6 ]">
-			<p class="[ text-uppercase ]"><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</strong></p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt neque, fugiat nostrum. Tempora aliquid quasi blanditiis doloribus deserunt natus obcaecati non. Recusandae ipsa fugit molestias error eum repellat qui omnis.</p>
-		</div>
-		<div class="[ col-xs-12 col-sm-6 ]">
+		<div class="[ col-xs-12 col-sm-6 ][ margin-bottom ]">
 			<p class="[ text-uppercase ]"><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</strong></p>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt neque, fugiat nostrum. Tempora aliquid quasi blanditiis doloribus deserunt natus obcaecati non. Recusandae ipsa fugit molestias error eum repellat qui omnis.</p>
 		</div>
 	</div>
-		<div class="[ row ]">
-		<div class="[ col-xs-12 col-sm-6 ]">
+	<div class="[ row ]">
+		<div class="[ col-xs-12 col-sm-6 ][ margin-bottom ]">
 			<p class="[ text-uppercase ]"><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</strong></p>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt neque, fugiat nostrum. Tempora aliquid quasi blanditiis doloribus deserunt natus obcaecati non. Recusandae ipsa fugit molestias error eum repellat qui omnis.</p>
 		</div>
-		<div class="[ col-xs-12 col-sm-6 ]">
+		<div class="[ col-xs-12 col-sm-6 ][ margin-bottom ][ hidden-xs ]">
+			<p class="[ text-uppercase ]"><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</strong></p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt neque, fugiat nostrum. Tempora aliquid quasi blanditiis doloribus deserunt natus obcaecati non. Recusandae ipsa fugit molestias error eum repellat qui omnis.</p>
+		</div>
+	</div>
+	<div class="[ row ]">
+		<div class="[ col-xs-12 col-sm-6 ][ margin-bottom ][ hidden-xs ]">
+			<p class="[ text-uppercase ]"><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</strong></p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt neque, fugiat nostrum. Tempora aliquid quasi blanditiis doloribus deserunt natus obcaecati non. Recusandae ipsa fugit molestias error eum repellat qui omnis.</p>
+		</div>
+		<div class="[ col-xs-12 col-sm-6 ][ margin-bottom ][ hidden-xs ]">
 			<p class="[ text-uppercase ]"><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</strong></p>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt neque, fugiat nostrum. Tempora aliquid quasi blanditiis doloribus deserunt natus obcaecati non. Recusandae ipsa fugit molestias error eum repellat qui omnis.</p>
 		</div>

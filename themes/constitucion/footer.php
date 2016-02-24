471ef6c3c6297d130f3cdd7124a47d75970aae07
Nayeli Jordan <raul@zamacona.me>
2016-02-24 14:03:53 -0600

    script para modal

--- themes/constitucion/footer.php
+++ themes/constitucion/footer.php
@@ -2,6 +2,10 @@
 			<footer>
 				<img class="[ width-20 ][ center-block ]" src="<?php echo THEMEPATH; ?>images/logocdmx2.png">
 			</footer>
+
+			<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+			<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+			<script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.0/js/bootstrap.min.js"></script>
 		<?php wp_footer(); ?>
 	</body>
 

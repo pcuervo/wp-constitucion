72009057ccf2dbcb9e1e63223fccb1fe77109b00
Nayeli Jordan <raul@zamacona.me>
2016-03-03 17:35:33 -0600

    primera parte pdf certificado

--- themes/constitucion/templates/htmlPdfCertificate.php
+++ themes/constitucion/templates/htmlPdfCertificate.php
@@ -6,9 +6,30 @@
 		<link href="http://fonts.googleapis.com/css?family=Roboto:300,500,400,700" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" href="<?php echo CSSPATH; ?>styles-pdf.css" type="text/css"/>
 	</head>
+	<style>
+		.text-center{text-align: center;}
+		.img-logo{ width: 120px; }
+		.color-primary{ color: #e80e8a; }
+		.col{
+			float: left;
+			padding-right: 15px;padding-right: 15px;
+		}
+		.col-1{ width: 40%;}
+		.col-2{ width: 60%;}
+		.text-uppercase{ text-transform: uppercase; }
+	</style>
 	<body>
-		<h4>Hola PDF</h4>
-		<p>aqui maquetar el certificado</p>
-
+		<div class="[ col col-1 ]">
+			<img class="[ img-logo ]" src="<?php echo THEMEPATH; ?>images/consti_vertical.png">
+			<p>Con el folio núm. <span class="[ color-primary ]">000000</span><br>
+			podrás darle seguimiento <br>
+			a tu participación</p>
+		</div>
+		<div class="[ col col-2 ]">
+			<h3 class="[ text-uppercase ]">Diego emilio cuesy edgar</h3>
+			<p>El grupo de trabajo redactor del proyecto de constitución Política de la Ciudad de México otorga la presente constancia por haber contribuido con ideas, reflexiones y propuestas.</p>
+			<p>La Ciudad de México somos todas y todos. Gracias por ser parte de este proceso histórico.</p>
+			<p class="[ text-uppercase ]">Ciudad de México a 2 de marzo de 2016</p>
+		</div>
 	</body>
 </html>

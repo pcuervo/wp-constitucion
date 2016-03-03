8d2f4ff5f8e23f0bb17d39bdb4ed3e71ab8ad03a
maichomper <maichomper@gmail.com>
2016-03-03 17:38:30 -0600

    poner botones para checkboxes cuestionario

--- plugins/cuestionario-sondeo-ciudadano/classes/class-sondeo_cdmx-survey.php
+++ plugins/cuestionario-sondeo-ciudadano/classes/class-sondeo_cdmx-survey.php
@@ -201,7 +201,14 @@ class Sondeo_CDMX_Survey {
 				</div>
 				<form id="myform" class="fs-form fs-form-full" autocomplete="off">
 					<ol class="fs-fields">	
-						
+						<li id="js-grandes-retos" data-question="26">
+							<label class="[ fs-field-label fs-anim-upper ][ color-gray ]" for="grandes-retos">Si pensaras en los grandes retos de esta Ciudad, ¿cuáles son los primeros cuatro que te llegan a la mente?</label>
+							<?php $grandes_retos = $this->get_options_grandes_retos(); ?>
+							<?php foreach ( $grandes_retos as $reto ) : ?>
+								<a class="[ btn btn-tertiary ]"><?php echo $reto; ?></a>
+							<?php endforeach; ?>
+							<input class="[ fs-anim-lower ]" id="q9" name="grandes-retos" type="text" required/>
+						</li>
 						<li id="js-donde-vives" data-question="1">
 							<label class="[ fs-field-label fs-anim-upper ][ color-gray ]">¿En dónde vives?</label>
 							<select class="[ cs-select cs-skin-boxes ][ fs-anim-lower ]" required="required">

80a5eea59186a1d90cb664346ae4cec5dfc8ca77
Nayeli Jordan <raul@zamacona.me>
2016-03-03 12:39:26 -0600

    secciones con texto de constitución cdmx

--- themes/constitucion/page-constitucion-cdmx.php
+++ themes/constitucion/page-constitucion-cdmx.php
@@ -16,7 +16,7 @@ $img_constitucion_cdmx = get_option( 'img_constitucion_cdmx' ); ?>
 </section>
 
 <section class="[ container ]" id="jefe_gobierno" >
-	<h2 class="[ margin-bottom ]"></h2>
+	<h2>Mensaje del jefe de gobierno</h2>
 	<div class="[ row ]">
 		<div class="[ col-xs-12 ][ col-sm-offset-1 col-sm-10 ]">
 			<div class="[ content-mensaje-jefegob ]">
@@ -40,31 +40,30 @@ $img_constitucion_cdmx = get_option( 'img_constitucion_cdmx' ); ?>
 </section>
 
 <section class="[ container ]" id="sobre_constitucion">
-	<div>
-		<div class="[ row ]">
-			<div class="[ col-xs-12 ][ col-sm-offset-1 col-sm-10 ]">
+	<h2>Sobre la constitución</h2>
+	<div class="[ row ]">
+		<div class="[ col-xs-12 ][ col-sm-offset-1 col-sm-10 ]">
 
-				<div class="[ content-acerca-de ]">
-					<blockquote>
-						<p>Sed dignissim ligula id justo feugiat sodales.Mauris eu est arcu. Donec vestibulum rutrum feugiat sodales auris est.</p>
-					</blockquote>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-						accumsan lorem id porttitor porttitor. In pulvinar pellentesque ornare. Morbi facilisis sem molestie sapien euismod,
-						vel ornare mi sagittis. Nulla at porta arcu, ut lacinia dui. In semper.Praesent quis enim mollis, molestie nisl non, dapibus tellus. Vestibu-lum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed dignissim ligula id justo feugiat sodales. Mauris eu est arcu. Donec vestibulum rutrum arcu, quis molestie odio iaculis in. Nunc at rutrum risus.
-					</p>
-				</div>
-
-				<div class="[ content-acerca-de ]" >
-					<blockquote>
-						<p>Sed dignissim ligula id justo feugiat sodales.Mauris eu est arcu. Donec vestibulum rutrum feugiat sodales auris est.</p>
-					</blockquote>
-					<p>
-						Praesent quis enim mollis, molestie nisl non, dapibus tellus. Vestibu-lum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed dignissim ligula id justo feugiat sodales. Mauris eu est arcu. Donec vestibulum rutrum arcu, quis molestie odio iaculis in. Nunc at rutrum risus.
-					</p>
-				</div>
-
+			<div class="[ content-acerca-de ]">
+				<blockquote>
+					<p>Sed dignissim ligula id justo feugiat sodales.Mauris eu est arcu. Donec vestibulum rutrum feugiat sodales auris est.</p>
+				</blockquote>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+					accumsan lorem id porttitor porttitor. In pulvinar pellentesque ornare. Morbi facilisis sem molestie sapien euismod,
+					vel ornare mi sagittis. Nulla at porta arcu, ut lacinia dui. In semper.Praesent quis enim mollis, molestie nisl non, dapibus tellus. Vestibu-lum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed dignissim ligula id justo feugiat sodales. Mauris eu est arcu. Donec vestibulum rutrum arcu, quis molestie odio iaculis in. Nunc at rutrum risus.
+				</p>
 			</div>
+
+			<div class="[ content-acerca-de ]" >
+				<blockquote>
+					<p>Sed dignissim ligula id justo feugiat sodales.Mauris eu est arcu. Donec vestibulum rutrum feugiat sodales auris est.</p>
+				</blockquote>
+				<p>
+					Praesent quis enim mollis, molestie nisl non, dapibus tellus. Vestibu-lum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed dignissim ligula id justo feugiat sodales. Mauris eu est arcu. Donec vestibulum rutrum arcu, quis molestie odio iaculis in. Nunc at rutrum risus.
+				</p>
+			</div>
+
 		</div>
 	</div>
 </section>
@@ -101,77 +100,7 @@ $img_constitucion_cdmx = get_option( 'img_constitucion_cdmx' ); ?>
 </section>
 
 
-<article class="[ bg-gray-light ][ margin-top-bottom--large padding--top-bottom--xlarge ]">
-	<section class="[ container padding--sides--xsm ]">
-		<?php $proceso = get_page_by_path('proceso-participativo'); ?>
-		<h2 class="[ no-margin-top ]"><?php echo $proceso->post_title; ?></h2>
-		<?php echo $proceso->post_content; ?>
 
-		<div class="[ row ][ margin-top-bottom ]">
-			<div class="[ col-xs-6 col-sm-3 ]">
-				<div class="[ row ]">
-					<div class="[ col-xs-8 ]">
-						<p class="[ fz-xlarge ]">Paso 1</p>
-						<p class="[ fz-small ]"><?php echo get_post_meta($proceso->ID, 'paso_a', true);?></p>
-					</div>
-					<div class="[ col-xs-4 no-padding--sides ]">
-						<img class="[ img-responsive ][ margin-auto ]" src="<?php echo THEMEPATH ?>images/flecha.png" alt="" /></div>
-					</div>
-				</div>
-
-			<div class="[ col-xs-6 col-sm-3 ]">
-				<div class="[ row ]">
-					<div class="[ col-xs-8 ]">
-						<p class="[ fz-xlarge ]">Paso 2</p>
-						<p class="[ fz-small ]"><?php echo get_post_meta($proceso->ID, 'paso_b', true);?></p>
-
-					</div>
-					<div class="[ col-xs-4 no-padding--sides ]">
-						<img class="[ img-responsive ][ margin-auto ]" src="<?php echo THEMEPATH ?>images/flecha.png" alt="" />
-					</div>
-				</div>
-			</div>
-			<div class="[ col-xs-6 col-sm-3 ]">
-				<div class="[ row ]">
-					<div class="[ col-xs-8 ]">
-						<p class="[ fz-xlarge ]">Paso 3</p>
-						<p class="[ fz-small ]"><?php echo get_post_meta($proceso->ID, 'paso_c', true);?></p>
-					</div>
-					<div class="[ col-xs-4 no-padding--sides ]">
-						<img class="[ img-responsive ][ margin-auto ]" src="<?php echo THEMEPATH ?>images/flecha.png" alt="" />
-					</div>
-				</div>
-			</div>
-			<div class="[ col-xs-6 col-sm-3 ]">
-				<div class="[ row ]">
-					<div class="[ col-xs-8 ]">
-						<p class="[ fz-xlarge ]">Paso 4</p>
-						<p class="[ fz-small ]"><?php echo get_post_meta($proceso->ID, 'paso_d', true);?></p>
-					</div>
-					<div class="[ col-xs-4 no-padding--sides ]">
-						<img class="[ img-responsive ][ margin-auto ]" src="<?php echo THEMEPATH ?>images/flecha.png" alt="" /></div>
-					</div>
-				</div>
-		</div>
-
-		<div>
-			<?php $attachment = get_attachment_pdf($proceso->ID);
-			if ( $attachment != '' ) : ?>
-				<a href="<?php echo $attachment; ?>" target="_blank" class="descargar-doc">
-			<?php endif; ?>
-				<p class="[ inline-block middle ][ text-uppercase ]">Descarga los documentos oficiales aquí</p>
-				<img class="[ svg icon icon--iconed icon--thickness-1 icon--stroke ][ color-primary ]"  src="<?php echo THEMEPATH; ?>icons/file-pdf.svg">
-			<?php if ( $attachment != '' ) echo '</a>'; ?>
-		</div>
-	</section>
-</article>
-
-<?php $experiencias = get_page_by_path('experiencias-y-buenas-practicas-globales');
-$url_image_experiencias = attachment_image_url( $experiencias->ID, 'full' );?>
-<section class="[ container padding--sides--xsm ][ margin-bottom--large ]">
-	<h2><?php echo $experiencias->post_title; ?></h2>
-	<?php echo $experiencias->post_content; ?>
-</section>
 <section class="[ container ]" id="grupo_trabajo">
 	<h2 class="[ margin-bottom ]">Grupo de trabajo</h2>
 	<div class="[ row ]">
@@ -360,50 +289,182 @@ $url_image_experiencias = attachment_image_url( $experiencias->ID, 'full' );?>
 		</div>
 	</div>
 </section>
-<section class="[ container ]" id="sobre_plataforma">
-	<h2 class="[ margin-bottom ]">Sobre la plataforma</h2>
+<article class="[ bg-gray--fondo padding--top-bottom ]">
+	<section class="[ container ]" id="sobre_plataforma">
+		<h2>Sobre la plataforma</h2>
+		<div class="[ row ]">
+			<div class="[ col-xs-12 ][ col-sm-offset-1 col-sm-10 ]">
+
+				<div class="[ content-acerca-de ]">
+					<blockquote>
+						<p>Sed dignissim ligula id justo feugiat sodales.Mauris eu est arcu. Donec vestibulum rutrum feugiat sodales auris est.</p>
+					</blockquote>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+						accumsan lorem id porttitor porttitor. In pulvinar pellentesque ornare. Morbi facilisis sem molestie sapien euismod,
+						vel ornare mi sagittis. Nulla at porta arcu, ut lacinia dui. In semper.Praesent quis enim mollis, molestie nisl non, dapibus tellus. Vestibu-lum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed dignissim ligula id justo feugiat sodales. Mauris eu est arcu. Donec vestibulum rutrum arcu, quis molestie odio iaculis in. Nunc at rutrum risus.
+					</p>
+				</div>
+
+				<div class="[ content-acerca-de ]" >
+					<blockquote>
+						<p>Sed dignissim ligula id justo feugiat sodales.Mauris eu est arcu. Donec vestibulum rutrum feugiat sodales auris est.</p>
+					</blockquote>
+					<p>
+						Praesent quis enim mollis, molestie nisl non, dapibus tellus. Vestibu-lum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed dignissim ligula id justo feugiat sodales. Mauris eu est arcu. Donec vestibulum rutrum arcu, quis molestie odio iaculis in. Nunc at rutrum risus.
+					</p>
+				</div>
+
+			</div>
+		</div>
+	</section>
+</article>
+<section class="[ descanso-visual ]">
+	<img src="<?php echo THEMEPATH; ?>images/cd1.png" alt="">
 </section>
 <section class="[ container ]" id="hacia_constituyente">
-	<h2 class="[ margin-bottom ]">Hacia el constituyente</h2>
-</section>
-<section class="[ container ]" id="constitución_wiki">
-	<h2 class="[ margin-bottom ]">Constitución en wiki</h2>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit in suscipit similique tempore ab quam voluptatibus, quos modi fuga iusto earum incidunt repellendus quis tempora? Perspiciatis officia accusantium temporibus esse.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum possimus dolorum quam illo veniam id placeat nulla repellat vero debitis ratione consectetur eos praesentium, delectus doloremque dolor, ipsa deserunt eaque.</p>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem consequuntur error illum. Atque esse, et iure rerum fugiat consequuntur repellendus est dignissimos alias officia fugit culpa nam consequatur exercitationem quae!</p>
-	<div class="[ text-center ][ margin-top--large ]">
-		<a class="[ btn btn-primary btn-large ][ margin-bottom--large ]" data-toggle="modal" data-target="#trabajador"><strong>Visitar sitio</strong></a>
-	</div>
-</section>
-<section class="[ bg-image rectangle ][ margin-bottom ][ relative ]" style="background-image: url('<?php echo $url_image_experiencias; ?>');"></section>
-<section class="[ container padding--sides--xsm ][ margin-bottom--large ]" id="preguntas_frecuentes">
-	<h2 class="[ margin-bottom ]">Preguntas frecuentes</h2>
-	<?php $preguntas = new WP_Query(array(
-			'post_type'      => 'preguntas-frecuentes',
-			'posts_per_page' => -1,
-		));
+	<h2>Hacia el constituyente</h2>
+	<div class="[ row ]">
+		<div class="[ col-xs-12 ][ col-sm-offset-1 col-sm-10 ]">
 
-	if ( $preguntas->have_posts() ) :
-		$count = 0;
-		while ( $preguntas->have_posts() ) : $preguntas->the_post();
-			if($count%2 == 0):
-			    echo $count > 0 ? '</div>' : '';
-			    echo '<div class="[ row ]">';
-			endif; ?>
-
-			<div class="[ col-xs-12 col-sm-6 ][ margin-bottom ]">
-				<p class="[ text-uppercase ]"><strong><?php the_title(); ?></strong></p>
-				<?php the_content(); ?>
+			<div class="[ content-acerca-de ]">
+				<blockquote>
+					<p>Sed dignissim ligula id justo feugiat sodales.Mauris eu est arcu. Donec vestibulum rutrum feugiat sodales auris est.</p>
+				</blockquote>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+					accumsan lorem id porttitor porttitor. In pulvinar pellentesque ornare. Morbi facilisis sem molestie sapien euismod,
+					vel ornare mi sagittis. Nulla at porta arcu, ut lacinia dui. In semper.Praesent quis enim mollis, molestie nisl non, dapibus tellus. Vestibu-lum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed dignissim ligula id justo feugiat sodales. Mauris eu est arcu. Donec vestibulum rutrum arcu, quis molestie odio iaculis in. Nunc at rutrum risus.
+				</p>
 			</div>
 
-			<?php $count++;
-		endwhile;
+			<div class="[ content-acerca-de ]" >
+				<blockquote>
+					<p>Sed dignissim ligula id justo feugiat sodales.Mauris eu est arcu. Donec vestibulum rutrum feugiat sodales auris est.</p>
+				</blockquote>
+				<p>
+					Praesent quis enim mollis, molestie nisl non, dapibus tellus. Vestibu-lum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed dignissim ligula id justo feugiat sodales. Mauris eu est arcu. Donec vestibulum rutrum arcu, quis molestie odio iaculis in. Nunc at rutrum risus.
+				</p>
+			</div>
 
-	endif;
-	wp_reset_postdata(); ?>
+		</div>
+	</div>
 </section>
+<article class="[ bg-gray--fondo padding--top-bottom ]">
+	<section class="[ container ]" id="constitución_wiki">
+		<h2 class="[ margin-bottom ]">Constitución en wiki</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit in suscipit similique tempore ab quam voluptatibus, quos modi fuga iusto earum incidunt repellendus quis tempora? Perspiciatis officia accusantium temporibus esse.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum possimus dolorum quam illo veniam id placeat nulla repellat vero debitis ratione consectetur eos praesentium, delectus doloremque dolor, ipsa deserunt eaque.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem consequuntur error illum. Atque esse, et iure rerum fugiat consequuntur repellendus est dignissimos alias officia fugit culpa nam consequatur exercitationem quae!</p>
+		<div class="[ text-center ][ margin-top--large ]">
+			<a class="[ btn btn-primary btn-large ][ margin-bottom--large ]" data-toggle="modal" data-target="#trabajador"><strong>Visitar sitio</strong></a>
+		</div>
+	</section>
+</article>
+<section class="[ bg-image rectangle ][ margin-bottom ][ relative ]" style="background-image: url('<?php echo $url_image_experiencias; ?>');"></section>
+<article class="[ bg-gray--fondo padding--top-bottom ]">
+	<section class="[ container padding--sides--xsm ][ margin-bottom--large ]" id="preguntas_frecuentes">
+		<h2 class="[ margin-bottom ]">Preguntas frecuentes</h2>
+		<?php $preguntas = new WP_Query(array(
+				'post_type'      => 'preguntas-frecuentes',
+				'posts_per_page' => -1,
+			));
+
+		if ( $preguntas->have_posts() ) :
+			$count = 0;
+			while ( $preguntas->have_posts() ) : $preguntas->the_post();
+				if($count%2 == 0):
+				    echo $count > 0 ? '</div>' : '';
+				    echo '<div class="[ row ]">';
+				endif; ?>
+
+				<div class="[ col-xs-12 col-sm-6 ][ margin-bottom ]">
+					<p class="[ text-uppercase ]"><strong><?php the_title(); ?></strong></p>
+					<?php the_content(); ?>
+				</div>
+
+				<?php $count++;
+			endwhile;
+
+		endif;
+		wp_reset_postdata(); ?>
+	</section>
+</article>
 <section class="[ container ]" id="biblioteca" >
 	<h2 class="[ margin-bottom ]" id="biblioteca">Biblioteca</h2>
 
+</section>
+
+
+<article class="[ bg-gray-light ][ margin-top-bottom--large padding--top-bottom--xlarge ]">
+	<section class="[ container padding--sides--xsm ]">
+		<?php $proceso = get_page_by_path('proceso-participativo'); ?>
+		<h2 class="[ no-margin-top ]"><?php echo $proceso->post_title; ?></h2>
+		<?php echo $proceso->post_content; ?>
+
+		<div class="[ row ][ margin-top-bottom ]">
+			<div class="[ col-xs-6 col-sm-3 ]">
+				<div class="[ row ]">
+					<div class="[ col-xs-8 ]">
+						<p class="[ fz-xlarge ]">Paso 1</p>
+						<p class="[ fz-small ]"><?php echo get_post_meta($proceso->ID, 'paso_a', true);?></p>
+					</div>
+					<div class="[ col-xs-4 no-padding--sides ]">
+						<img class="[ img-responsive ][ margin-auto ]" src="<?php echo THEMEPATH ?>images/flecha.png" alt="" /></div>
+					</div>
+				</div>
+
+			<div class="[ col-xs-6 col-sm-3 ]">
+				<div class="[ row ]">
+					<div class="[ col-xs-8 ]">
+						<p class="[ fz-xlarge ]">Paso 2</p>
+						<p class="[ fz-small ]"><?php echo get_post_meta($proceso->ID, 'paso_b', true);?></p>
+
+					</div>
+					<div class="[ col-xs-4 no-padding--sides ]">
+						<img class="[ img-responsive ][ margin-auto ]" src="<?php echo THEMEPATH ?>images/flecha.png" alt="" />
+					</div>
+				</div>
+			</div>
+			<div class="[ col-xs-6 col-sm-3 ]">
+				<div class="[ row ]">
+					<div class="[ col-xs-8 ]">
+						<p class="[ fz-xlarge ]">Paso 3</p>
+						<p class="[ fz-small ]"><?php echo get_post_meta($proceso->ID, 'paso_c', true);?></p>
+					</div>
+					<div class="[ col-xs-4 no-padding--sides ]">
+						<img class="[ img-responsive ][ margin-auto ]" src="<?php echo THEMEPATH ?>images/flecha.png" alt="" />
+					</div>
+				</div>
+			</div>
+			<div class="[ col-xs-6 col-sm-3 ]">
+				<div class="[ row ]">
+					<div class="[ col-xs-8 ]">
+						<p class="[ fz-xlarge ]">Paso 4</p>
+						<p class="[ fz-small ]"><?php echo get_post_meta($proceso->ID, 'paso_d', true);?></p>
+					</div>
+					<div class="[ col-xs-4 no-padding--sides ]">
+						<img class="[ img-responsive ][ margin-auto ]" src="<?php echo THEMEPATH ?>images/flecha.png" alt="" /></div>
+					</div>
+				</div>
+		</div>
+
+		<div>
+			<?php $attachment = get_attachment_pdf($proceso->ID);
+			if ( $attachment != '' ) : ?>
+				<a href="<?php echo $attachment; ?>" target="_blank" class="descargar-doc">
+			<?php endif; ?>
+				<p class="[ inline-block middle ][ text-uppercase ]">Descarga los documentos oficiales aquí</p>
+				<img class="[ svg icon icon--iconed icon--thickness-1 icon--stroke ][ color-primary ]"  src="<?php echo THEMEPATH; ?>icons/file-pdf.svg">
+			<?php if ( $attachment != '' ) echo '</a>'; ?>
+		</div>
+	</section>
+</article>
+
+<?php $experiencias = get_page_by_path('experiencias-y-buenas-practicas-globales');
+$url_image_experiencias = attachment_image_url( $experiencias->ID, 'full' );?>
+<section class="[ container padding--sides--xsm ][ margin-bottom--large ]">
+	<h2><?php echo $experiencias->post_title; ?></h2>
+	<?php echo $experiencias->post_content; ?>
 </section>
 
 <?php get_footer(); ?>

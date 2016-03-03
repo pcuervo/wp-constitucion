bcac30a0ee864c0b1ee9dc1dd6ec48df24b06721
arisbeth <arisbeth.hg@gmail.com>
2016-03-03 16:11:45 -0600

    formulario dialogos c estilos

--- themes/constitucion/page-participa.php
+++ themes/constitucion/page-participa.php
@@ -286,44 +286,44 @@ if( isset( $result['success'] ) ): ?>
                 <p>En esta sección podrás registrar acontecimientos que sean espacios de diálogo relacionados a la construcción de la Constitución de la Ciudad de México. Estos pueden ser sesiones de trabajo colaborativas, foros, asambleas, talleres o conferencias.</p>
             </div>
         </div>
-        <div class="[ row ]">
-            <div class="[ border-box-shadow ][ margin-sides--large margin-bottom--large padding--large ]">
-                <h2>Formato y formulario para registrar eventos</h2>
+        <div class="[ row ][ border-box-shadow ][ margin-sides--large margin-bottom--large padding--large ]">
+                <h2 class="[ margin-bottom--large ]">Formato y formulario para registrar eventos</h2>
                 <form action="">
-                    <div class="[ form-group ][ col-xs-12 col-sm-6 ]">
+                    <div class="[ col-xs-12 col-sm-6 ]">
                         <label for=""><strong class="[ color-primary ]">Nombre del acontecimiento </strong>(máximo 100 caracteres)</label>
-                        <input input type="text" class="[ form-control ][ input-primary border-gray ][ margin-bottom--large ]" placeholder="Text input">
-                        <label for="">Frase que describe el acontecimiento Comparte una frase que describa el evento/sesión de trabajo/diálogo público (120 caracteres) </label>
-                        <input input type="text" class="[ form-control ][ input-primary border-gray ][ margin-bottom--large ]"placeholder="Text input">
-                        <label for="">Descripción Describe los objetivos, alcances y perfil de asistentes de tu acontecimiento  (120 palabras) </label>
-                        <input input type="text" class="[ form-control ][ input-primary border-gray ][ margin-bottom--large ]" placeholder="Text input">
-                        <label for="">Liga oficial (Campo No obligatorio) Copia a continuación la liga del sitio oficial del acontecimiento, la cuenta de Facebook o de Twitter </label>
-                        <input input type="text" class="[ form-control ][ input-primary border-gray ][ margin-bottom--large ]"placeholder="Text input">
-                        <label for="">Fechas (Puede ser un sólo día) </label>
-                        <input type="date" name="fecha">
-                        <label for="">Ubicación Señala abajo dónde se llevara a cabo </label>
-                        <input input type="text" class="[ form-control ][ input-primary border-gray ][ margin-bottom--large ]"placeholder="Text input">
+                        <input input type="text" class="[ form-control ][ input-primary border-gray ][ margin-bottom--large ]" placeholder="Nombre del acontecimiento">
+                        <label for=""><strong class="[ color-primary ]">Frase que describe el acontecimiento</strong> Comparte una frase que describa el evento/sesión de trabajo/diálogo público (120 caracteres) </label>
+                        <input input type="text" class="[ form-control ][ input-primary border-gray ][ margin-bottom--large ]"placeholder="Frase que describe el acontecimiento">
+                        <label for=""><strong class="[ color-primary ]">Descripción Describe los objetivos, alcances y perfil de asistentes de tu acontecimiento</strong>  (120 palabras) </label>
+                        <input input type="text" class="[ form-control ][ input-primary border-gray ][ margin-bottom--large ]" placeholder="Descripción Describe los objetivos, alcances y perfil de asistentes de tu acontecimiento">
+                        <label for=""><strong class="[ color-primary ]">Liga oficial</strong> (Campo No obligatorio) Copia a continuación la liga del sitio oficial del acontecimiento, la cuenta de Facebook o de Twitter </label>
+                        <input input type="text" class="[ form-control ][ input-primary border-gray ][ margin-bottom--large ]"placeholder="Liga oficial">
+                        <label for=""><strong class="[ color-primary ]">Fechas</strong> (Puede ser un sólo día) </label>
+                        <input class="[ input-primary border-gray ][ margin-bottom--large ]" type="date" name="fecha">
+                        <label for=""><strong class="[ color-primary ]">Ubicación</strong> Señala abajo dónde se llevara a cabo </label>
+                        <input input type="text" class="[ form-control ][ input-primary border-gray ][ margin-bottom--large ]"placeholder="Ubicación">
                     </div>
-                    <div class="[ form-group ][ col-xs-12 col-sm-6 ]">
-                        <label for="">Horario ¿En qué momentos se llevará a cabo el evento? </label>
-                        <input input type="text" class="[ form-control ][ input-primary border-gray ][ margin-bottom--large ]"placeholder="Text input">
-                        <label for="">Institución u organización. Si este acontecimiento es organizado por una institución u organización, señala abajo cuál. En caso de más de una, separar por comas. Si son un grupo de personas, nombren su equipo de trabajo. </label>
-                        <input input type="text" class="[ form-control ][ input-primary border-gray ][ margin-bottom--large ]"placeholder="Text input">
-                        <label for="">Asistentes </label>
-                        <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
-                        La asistencia es libre
-                        <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
-                        por invitación
-                        <label for="">Fotografía de cartel o publicidad del acontecimiento. (Campo No obligatorio) Coloca el archivo de la fotografía en google drive, dropbox o onedrive y a continuación copia la liga.</label>
-                        <input input type="text" class="[ form-control ][ input-primary border-gray ][ margin-bottom--large ]"placeholder="Text input">
-                        <label for="">Correo Electrónico Señala a continuación un correo electrónico</label>
-                       <input type="email" class="form-control" id="" placeholder="Email">
+                    <div class="[ col-xs-12 col-sm-6 ]">
+                    	<label for=""><strong class="[ color-primary ]">Horario</strong> ¿En qué momentos se llevará a cabo el evento? </label>
+                        <input input type="text" class="[ form-control ][ input-primary border-gray ][ margin-bottom--large ]"placeholder="Horario">
+                        <label for=""><strong class="[ color-primary ]">Institución u organización.</strong> Si este acontecimiento es organizado por una institución u organización, señala abajo cuál. En caso de más de una, separar por comas. Si son un grupo de personas, nombren su equipo de trabajo. </label>
+                        <input input type="text" class="[ form-control ][ input-primary border-gray ][ margin-bottom--large ]"placeholder="Institución u organización.">
+                        <label for=""><strong class="[ color-primary ]">Asistentes</strong></label>
+                        <div class="[ margin-bottom--large ]">
+							<input  type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+							La asistencia es libre
+							<input  type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+							por invitación
+                        </div>
+                        <label for=""><strong class="[ color-primary ]">Fotografía de cartel o publicidad del acontecimiento.</strong> (Campo No obligatorio) Coloca el archivo de la fotografía en google drive, dropbox o onedrive y a continuación copia la liga.</label>
+    					<input class="[ form-control ][ input-primary border-gray ][ margin-bottom--large ]" type="file" id="exampleInputFile">
+                        <label for=""><strong class="[ color-primary ]">Correo Electrónico</strong> Señala a continuación un correo electrónico</label>
+                       <input type="email" class="[ form-control ][ input-primary border-gray ][ margin-bottom--large ]" id="" placeholder="Correo Electrónico">
                     </div>
-                    <button type="submit" class="[ btn btn-secondary ]">Submit</button>
-
+                    <div class="[ col-xs-12 text-center ]">
+                    	<button type="submit" class="[ btn btn-secondary ]">Submit</button>
+                    </div>
                 </form>
-            </div>
-
         </div>
 
     </section>

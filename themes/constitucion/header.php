a454694ae5e78354c741a0a89bc147030b4905d7
alejandro <alex@losmaquiladores.com>
2016-03-07 10:48:51 -0600

    icono twitter header

--- themes/constitucion/header.php
+++ themes/constitucion/header.php
@@ -230,7 +230,7 @@
 						</li><li
 						id="js-btn-cdmx">
 							<a href="#">
-								<img class="[ svg icon icon--iconed icon--thickness-1 icon--fill ][ color-gray ]" src="<?php echo THEMEPATH; ?>icons/twitter.svg">
+								<img class="[ svg icon icon--iconed--small icon--thickness-1 icon--fill ][ color-gray ]" src="<?php echo THEMEPATH; ?>icons/twitter.svg">
 								#SomosCDMX<!-- Contacto - page-contacto.php-->
 							</a>
 						</li>

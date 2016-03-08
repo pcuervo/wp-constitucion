ca3248b6f91e89ee5f551e67622c1cf3aaacff1c
alejandro <alex@losmaquiladores.com>
2016-03-08 15:14:11 -0600

    linea del tiempo home

--- themes/constitucion/page-home.php
+++ themes/constitucion/page-home.php
@@ -66,7 +66,7 @@
 			<ul id="scroll" class="[ scroll-cronologia ][ horizontal-slide ][ text-center ]">
 
 				<?php $cronologia = new WP_Query(array(
-						'post_type'      => 'linea-del-tiempo',
+						'post_type'      => 'eventos',
 						'posts_per_page' => -1,
 						'orderby'  => 'fecha_evento',
 						'order'    => 'ASC',
@@ -77,63 +77,79 @@
 
 						)
 					));
+				if ( ! empty ($cronologia->posts)):
+					$timeLine = new TimeLine;
+					$fechas = $timeLine->elementsTimeLine($cronologia->posts);
 
-				if ( $cronologia->have_posts() ) :
 					$count = 0;
 					$count_pasados = 0;
-					while ( $cronologia->have_posts() ) : $cronologia->the_post();
-						$tipo_evento = get_post_meta( $post->ID, 'tipo-de-evento', true );
-						$date_event = get_post_meta($post->ID, 'fecha_evento', true);
-						$count_pasados = strtotime($date_event) < strtotime( date('Y-m-d') ) ? $count_pasados + 1 : $count_pasados;
-						$class_status = strtotime($date_event) < strtotime( date('Y-m-d') ) ? 'ya-paso ' : '';
+					$data_attr = '';
+					$class_visibility = "";
+					foreach ($fechas as $date_events => $eventos):
+						$date_arr = getDateTransform($date_events);
+						$date = '<span class="[ color-primary ]"><strong>'. $date_arr[0] . '</strong></span> ' . $date_arr[1];
+						$count_pasados = strtotime($date_events) < strtotime( date('Y-m-d') ) ? $count_pasados + 1 : $count_pasados;
+						$class_status = strtotime($date_events) < strtotime( date('Y-m-d') ) ? 'ya-paso ' : '';
+						$count_events = count($eventos);
 
-						$date = '';
-						if ($date_event != '') {
-							$date_arr = getDateTransform($date_event);
-							$date = '<span class="[ color-primary ]"><strong>'. $date_arr[0] . '</strong></span> ' . $date_arr[1];
-						}
-
-
-						if($tipo_evento != 3): ?>
-						    <li class="[ box-li <?php echo $class_status; ?> ]">
-
-						    	<a href="<?php echo get_permalink(); ?>">
+						if ($count_events > 1): ?>
+							<li class="[ box-li mas-eventos <?php echo $class_status; ?> ]" data-date="<?php echo $date_events; ?>">
+						    	<a href="">
 									<p class="[ color-gray ][ fz-small top-date ][ margin-top--date ][ text-uppercase ]"><?php echo $date; ?></p>
 									<div class="[ border-gray--large ]"></div>
 									<div class="[ vertical-line ]"></div>
 									<div class="[ circle-xsmall ]"></div>
 									<div class="[ text--large relative ]">
-										<p class="[ evento evento-bottom ]"><?php the_title(); ?></p>
+										<p class="[ evento evento-bottom ]"><?php echo $count_events; ?> Eventos</p>
 									</div>
 								</a>
 							</li>
-						<?php else: ?>
 
-							<li class="[ box-li nota-destacada ]">
-								<div class="[ text--large text--destacado relative ]">
-									<p class="[ evento ]"><?php the_title(); ?></p>
-								</div>
-								<div class="[ box-li__lines ]">
-									<div class="[ vertical-line--large ]"></div>
-									<div class="[ border-gray--large ]"></div>
-								</div>
-								<p class="[ color-gray ][ fz-small ][ margin-bottom--date ][ text-uppercase ]"></p>
-							</li>
+							<?php $data_attr = $date_events;
+							$class_visibility = 'hidden ';
 
-						<?php endif;
+						endif;
 
-						$count++;
-					endwhile;
+						foreach ($eventos as $evento):
+
+							if((int)$evento->type != 3): ?>
+							    <li class="[ box-li <?php echo $class_status.$class_visibility; ?> ][ <?php echo 'date-'.$data_attr; ?> ]">
+							    	<a href="<?php echo get_permalink($evento->ID); ?>">
+										<p class="[ color-gray ][ fz-small top-date ][ margin-top--date ][ text-uppercase ]"><?php echo $date; ?></p>
+										<div class="[ border-gray--large ]"></div>
+										<div class="[ vertical-line ]"></div>
+										<div class="[ circle-xsmall ]"></div>
+										<div class="[ text--large relative ]">
+											<p class="[ evento evento-bottom ]"><?php echo $evento->post_title; ?></p>
+										</div>
+									</a>
+								</li>
+							<?php else: ?>
+								<li class="[ box-li nota-destacada ]">
+									<div class="[ text--large text--destacado relative ]">
+										<p class="[ evento ]"><?php echo $evento->post_title; ?></p>
+									</div>
+									<div class="[ box-li__lines ]">
+										<div class="[ vertical-line--large ]"></div>
+										<div class="[ border-gray--large ]"></div>
+									</div>
+									<p class="[ color-gray ][ fz-small ][ margin-bottom--date ][ text-uppercase ]"></p>
+								</li>
+							<?php endif;
+
+							$count++;
+						endforeach;
+						$data_attr = '';
+						$class_visibility = '';
+					endforeach;
 
 					$data = '';
-					if ($cronologia->found_posts >= 7 AND $count_pasados > 0):
-						$recorre = ($count_pasados - 1 ) * 165;
+					if ($cronologia->found_posts >= 7 AND $count_pasados > 0) {
+						$recorre = ($count_pasados - 1 ) * 163;
 						echo '<input type="hidden" id="recorre" value="'.$recorre.'">';
-					endif;
-				else:
-					echo '<p class="text-center">No existen eventos</p>';
-				endif;
-				wp_reset_postdata(); ?>
+					}
+				endif; ?>
+
 
 			</ul>
 		</div>

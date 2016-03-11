d409169742aca2b3afecdf57658c1556adc32efb
arisbeth <arisbeth.hg@gmail.com>
2016-03-10 18:10:24 -0600

    slide-cronica

--- themes/constitucion/js/functions.js
+++ themes/constitucion/js/functions.js
@@ -63,7 +63,7 @@
          */
         $('.slider.responsive').slick({
             dots: true,
-            infinite: false,
+            infinite: true,
             speed: 300,
             slidesToShow: 4,
             slidesToScroll: 4,

7c46ac2a57486ce49b598631eeb43a84cbbf1348
alejandro <alex@losmaquiladores.com>
2016-03-14 14:18:55 -0600

    integracion front pub pub

--- themes/constitucion/page-obtener-certificado.php
+++ themes/constitucion/page-obtener-certificado.php
@@ -1,8 +1,14 @@
-<?php get_header();?>
+<?php get_header();
+if( ! isset( $_GET['referencia'] ) || $_GET['referencia'] == '' ):
+	echo 'Referencia no registrada';
+else: 
+	$retos = new RetosParticipante($_GET['referencia']);
+	$retos = $retos->get_retos_relacionados(); ?>
+
 	<section class="[ container ][ margin-top--large ][ text-center ]">
 		<img class="[ img-logo ]" src="<?php echo THEMEPATH; ?>/images/logo-horizontal.gif" alt="Logo horizontal Constitución CDMX">
 		<h2 class="[ text-center text-uppercase ]">¡Gracias por participar!</h2>
-		<p class="[ text-center ][ js-codigo-referencia ]">Tu número de folio es: <span></span></p>
+		<p class="[ text-center ][ js-codigo-referencia ]">Tu número de folio es: <span><?php echo $_GET['referencia']; ?></span></p>
 		<p class="[ text-center text-uppercase ]">Escribe tu nombre y apellido para obtener tu certificado de participación</p>
 		<form class="[ row ][ margin-top--large ][ padding--sides--xsm ]" action="<?php echo site_url('pdf-certificado-de-participacion'); ?>" method="POST" data-parsley-certificado target="_blank">
 			<div class="[ col-xs-12 col-sm-4 ]">
@@ -11,7 +17,7 @@
 			<div class="[ col-xs-12 col-sm-4 ]">
 				<input type="text" name="apellidos" class="form-control [ margin-bottom--large ][ input-primary border-gray ]" placeholder="Apellidos" required="">
 			</div>
-			<input type="hidden" name="referencia">
+			<input type="hidden" name="referencia" value="<?php echo $_GET['referencia']; ?>">
 			<div class="[ col-xs-12 col-sm-4  ]">
 				<button type="submit" class="[ btn btn-secondary color-gray ][ center-block ]">obtener certificado</button>
 			</div>
@@ -20,56 +26,24 @@
 	<section class="[ container ]">
 		<h2 class="[ margin-bottom ]">Conoce los ensayos relacionados con los retos que elegiste</h2>
 		<div class="[ row ][ retos ]">
-			<div class="[ col-xs-12 col-sm-3 ][ margin-bottom--large ]">
-				<h3>Reto 1</h3><!-- reto 1 -->
-				<a href="">
-					<img class="[ svg icon icon--iconed icon--thickness-05 icon--stoke ][ color-gray ]"  src="<?php echo THEMEPATH; ?>icons/bookmark-article.svg" alt="icono de ensayos">
-					<p>Titulo del ensayo lorem ipsum</p>
-				</a>
-				<a href="">
-					<img class="[ svg icon icon--iconed icon--thickness-05 icon--stoke ][ color-gray ]"  src="<?php echo THEMEPATH; ?>icons/bookmark-article.svg" alt="icono de ensayos">
-					<p>Titulo del ensayo lorem ipsum sit amet</p>
-				</a>
-			</div>
-			<div class="[ col-xs-12 col-sm-3 ][ margin-bottom--large ]">
-				<h3>Reto 2</h3><!-- reto  2-->
-				<a href="">
-					<img class="[ svg icon icon--iconed icon--thickness-05 icon--stoke ][ color-gray ]"  src="<?php echo THEMEPATH; ?>icons/bookmark-article.svg" alt="icono de ensayos">
-					<p>Titulo del ensayo lorem</p>
-				</a>
-				<a href="">
-					<img class="[ svg icon icon--iconed icon--thickness-05 icon--stoke ][ color-gray ]"  src="<?php echo THEMEPATH; ?>icons/bookmark-article.svg" alt="icono de ensayos">
-					<p>Titulo del ensayo lorem ipsum sit</p>
-				</a>
-			</div>
-			<div class="[ col-xs-12 col-sm-3 ][ margin-bottom--large ]">
-				<h3>Reto 3</h3><!-- reto 3 -->
-				<a href="">
-					<img class="[ svg icon icon--iconed icon--thickness-05 icon--stoke ][ color-gray ]"  src="<?php echo THEMEPATH; ?>icons/bookmark-article.svg" alt="icono de ensayos">
-					<p>Titulo del ensayo lorem ipsum sit amet</p>
-				</a>
-				<a href="">
-					<img class="[ svg icon icon--iconed icon--thickness-05 icon--stoke ][ color-gray ]"  src="<?php echo THEMEPATH; ?>icons/bookmark-article.svg" alt="icono de ensayos">
-					<p>Titulo del ensayo</p>
-				</a>
-				<a href="">
-					<img class="[ svg icon icon--iconed icon--thickness-05 icon--stoke ][ color-gray ]"  src="<?php echo THEMEPATH; ?>icons/bookmark-article.svg" alt="icono de ensayos">
-					<p>Titulo del ensayo lorem ipsum sit amet</p>
-				</a>
-			</div>
-			<div class="[ col-xs-12 col-sm-3 ][ margin-bottom--large ]">
-				<h3>Reto 4</h3><!-- reto 4 -->
-				<a href="">
-					<img class="[ svg icon icon--iconed icon--thickness-05 icon--stoke ][ color-gray ]"  src="<?php echo THEMEPATH; ?>icons/bookmark-article.svg" alt="icono de ensayos">
-					<p>Titulo del ensayo</p>
-				</a>
-				<a href="">
-					<img class="[ svg icon icon--iconed icon--thickness-05 icon--stoke ][ color-gray ]"  src="<?php echo THEMEPATH; ?>icons/bookmark-article.svg" alt="icono de ensayos">
-					<p>Titulo del ensayo lorem ipsum sit amet lorem consectetur adipisicing elit.</p>
-				</a>
-			</div>
+			<?php if(! empty($retos)):
+				foreach ($retos as $key => $reto):
+					$term = get_term_by('id', $key, 'taxonomy-grandes-retos'); ?>
+					<div class="[ col-xs-12 col-sm-3 ][ margin-bottom--large ]">
+						<h3><?php echo $term->name ?></h3><!-- reto 1 -->
+						<?php foreach ($reto as $key => $pubpub): 
+							$url_pubpub = get_post_meta($pubpub->ID, 'url_pubpub', true); ?>
+							<a href="<?php echo $url_pubpub; ?>" target="_blank">
+								<img class="[ svg icon icon--iconed icon--thickness-05 icon--stoke ][ color-gray ]"  src="<?php echo THEMEPATH; ?>icons/bookmark-article.svg" alt="icono de ensayos">
+								<p><?php echo $pubpub->post_title; ?></p>
+							</a>
+						<?php endforeach; ?>
+					</div>
+				<?php endforeach;	
+			endif; ?>  
 		</div>
 	</section>
+<?php endif; ?>
 
 
 <?php get_footer(); ?>
